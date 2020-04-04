0401de1a4588fa9ea2b07d38f8ee4d8405cecdd3
Mishrule <mishrule1@gmail.com>
2020-04-04 22:20:36 +0000

    second commit

--- exact/urls.py
+++ exact/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from exact import views
 
 urlpatterns = [
-    path('exeat', views.assign_exeat, name='exeat'),
+    path('exeat', views.assign_exeat, name='assign_exeat'),
     path('view_exeat', views.view_exeat, name='view_exeat'),
     path('manage_exeat', views.manage_exeat, name='manage_exeat')
 ]
